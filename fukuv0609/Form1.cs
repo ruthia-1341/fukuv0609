3b5f506d794eeb3c2ec20670c93a97bb58734a56
Toya <66713857+NorauDn@users.noreply.github.com>
2022-06-09 14:01:32 +0900

    rikai

--- fukuv0609/Form1.cs
+++ fukuv0609/Form1.cs
@@ -22,12 +22,13 @@ namespace fukuv0609
         {
             MessageBox.Show("堀 朱莉");
             timer1.Enabled = false;
+            label1.Text = ("stop");
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left = label1.Left + (int.Parse(textBox1.Text));
-            label1.Top = label1.Top + (int.Parse(textBox2.Text));
+            label1.Left += (int.Parse(textBox1.Text));
+            label1.Top +=  (int.Parse(textBox2.Text));
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -43,21 +44,32 @@ namespace fukuv0609
         private void button2_Click(object sender, EventArgs e)
         {
             label1.Text = ("↓");
+            textBox2.Text = "5";
+            textBox1.Text = "0";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             label1.Text = ("→");
+        
+            textBox2.Text = "0";
+            textBox1.Text = "5";
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             label1.Text = ("←");
+        
+            textBox2.Text = "0";
+            textBox1.Text = "-5";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Text = ("↑");
+       
+            textBox2.Text = "-5";
+            textBox1.Text = "0";
         }
     }
 }
