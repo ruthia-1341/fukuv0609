7ac42269067a6cca114aad5aa8b78f6b84009d29
Toya <66713857+NorauDn@users.noreply.github.com>
2022-06-09 13:28:46 +0900

    a

--- fukuv0609/Form1.cs
+++ fukuv0609/Form1.cs
@@ -17,5 +17,25 @@ namespace fukuv0609
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("堀 朱莉");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + (int.Parse(textBox1.Text));
+            label1.Top = label1.Top + (int.Parse(textBox2.Text));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
